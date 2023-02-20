dc3bffbbe49f7e0133ac350b436818aea853f747
KenzeeKay <kenfordanderson@gmail.com>
2023-02-20 14:36:13 -0700

    updated windspeed

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let day = date.getDay();
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayWeatherCondition(response) {
   let cityElem = document.querySelector("#city");
   let tempElem = document.querySelector("#temperature");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
   cityElem.innerHTML = response.data.name;
   tempElem.innerHTML = Math.round(response.data.main.temp);
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = response.data.main.wind.speed;
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 function searchCity(city) {
