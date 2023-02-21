2f51e5391d9c87e2d0fec5eb89532a4263881ec9
KenzeeKay <kenfordanderson@gmail.com>
2023-02-21 15:33:41 -0700

    updated icon

--- script.js
+++ script.js
@@ -28,12 +28,17 @@ function displayWeatherCondition(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
   cityElem.innerHTML = response.data.name;
   tempElem.innerHTML = Math.round(response.data.main.temp);
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
 }
 function searchCity(city) {
   let apiKey = "4fed96bb993a5c8806dbb1374932ed84";
